caf3ef5eb981aec17192606b6e961f7f1f3aa663
saiful bahri <syaifulbachrimustamin@gmail.com>
2019-05-25 15:33:52 +0700

    update kata

--- cek_typo.py
+++ cek_typo.py
@@ -1,7 +1,8 @@
 #from jarowinkler import similarity as sim
 from pyjarowinkler import distance as sim
 import pandas as pd
-import os 
+import os
+import re
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 #open(dir_path + '/' + 'data.json')
@@ -35,7 +36,13 @@ def load_data():
 
 def getData(alamat):
     lineList = list()
-    with open(dir_path + '/' + alamat) as f:
+    with open(dir_path + '/' + alamat, encoding = "ISO-8859-1") as f:
+        for line in f:
+            lineList.append(line.rstrip('\n'))
+    return lineList
+def read_text(alamat):
+    lineList = list()
+    with open(alamat, encoding = "ISO-8859-1") as f:
         for line in f:
             lineList.append(line.rstrip('\n'))
     return lineList
@@ -56,14 +63,87 @@ if len(ganti_)!= len(kata_):
 def simJaro(kata1,kata2):
     return sim.get_jaro_distance(kata1, kata2, winkler=True, scaling=0.01)
 
-def norm_typo(komentar):
+def distinc_huruf(kata, jm=3):
+    #kata = just_get_text(kata)
+    if len(kata)==0:
+        return kata
+    n_kata = list()
+    if kata[0].isalpha() or kata[0].isnumeric():
+        pass
+    else:
+        if (kata[1].isalpha() or kata[1].isnumeric()) and kata[1] not in n_kata:
+            n_kata.append(kata[1])
+    if kata[0]=='a' or kata[0]=='m' or kata[0]=='p'or kata[0]=='b':#or kata[1]=='a' or kata[1]=='m' or kata[1]=='p':
+        if (kata[0].isalpha() or kata[0].isnumeric()):
+            n_kata.append(kata[0])
+        if kata[1] not in n_kata and (kata[1].isalpha() or kata[1].isnumeric()):
+            n_kata.append(kata[1])
+    for hr in kata:
+        if hr not in n_kata and hr != 'a' and hr != 'm' and hr != 'p'and hr != 'b':
+            if hr.isalpha() or hr.isnumeric():
+                n_kata.append(hr)
+    if len(n_kata)>jm:
+        return "".join(n_kata[:jm])
+    else:
+        return "".join(n_kata)
+    #if jm == 1:
+        #return "".join(n_kata[0])
+    #elif jm==2:
+        #return "".join(n_kata[:2])
+def new_corpus_k(kata, jm):
+    corpus = list()
+    #if len(kata)==0:
+        #return []
+    for i in distinc_huruf(kata, jm=jm):
+        x = i
+        i = "k_"+x+'_'
+        #j = "r_"+x+'_'
+        #print(i)
+        f = getData('data/kata/'+i+'.txt')
+        #f=open('data/kata/'+i+'.txt')
+        #f=f.read()
+        #f=f.split()
+        corpus+=f
+    return corpus
+def new_corpus_r(kata, jm):
+    corpus = list()
+    #if len(kata)==0:
+        #return []
+    for j in distinc_huruf(kata, jm=jm):
+        x = j
+        #i = "k_"+x+'_'
+        j = "r_"+x+'_'
+        #print(i)
+        f = getData('data/kata/'+j+'.txt')
+        #f=open('data/kata/'+i+'.txt')
+        #f=f.read()
+        #f=f.split()
+        corpus+=f
+    return corpus
+
+
+last_use_k = list()
+last_use_r = list()
+kbbi = getData('data/kata_kbbi_new.txt')
+def norm_typo(komentar, jm=3):
     if type(komentar)!=list:
         komentar_split = komentar.split()
     for indx, kt in enumerate(komentar_split):
+        kata_ = new_corpus_k(kt, jm)
+        ganti_ = new_corpus_r(kt, jm)
         cek = True
-        if kt in kata_:
+        if kt in kbbi:
+            #print("<<kbbi>>")
+            continue
+        elif kt in last_use_k:
+            last_use_k_index = last_use_k.index(kt)
+            komentar_split[indx] = last_use_r[last_use_k_index]
+            #print("<<last use>>")
+        elif kt in kata_:
             komentar_split[indx]= "" if ganti_[kata_.index(kt)] == 'nan' else ganti_[kata_.index(kt)]
-            print("<<<ada>>>")
+            last_use_k.append(kt)
+            last_use_r.append(komentar_split[indx])
+            #print("<<<ada>>>")
             #continue
         else:
             list_kemiripan = []
@@ -71,14 +151,20 @@ def norm_typo(komentar):
                 list_kemiripan.append(simJaro(kt, sl)) 
                 if simJaro(kt, sl) >= .96:
                     komentar_split[indx]="" if ganti_[ix] == 'nan' else ganti_[ix]
-                    print("similarity-> "+kt+"|"+sl+" ->", simJaro(kt, sl))
+                    #print("similarity-> "+kt+"|"+sl+" ->", simJaro(kt, sl))
+                    last_use_k.append(kt)
+                    last_use_r.append(komentar_split[indx])
                     cek = False
                     break
-            if max(list_kemiripan)>=.88 and cek== True:
+            if max(list_kemiripan)>=.90 and cek== True:
                 #print("similarity",kt, str(max(list_kemiripan)))
-                print("similarity =>",kt,"|",kata_[list_kemiripan.index(max(list_kemiripan))],"=>", str(max(list_kemiripan)))
+                #print("similarity =>",kt,"|",kata_[list_kemiripan.index(max(list_kemiripan))],"=>", str(max(list_kemiripan)))
                 komentar_split[indx]= "" if ganti_[list_kemiripan.index(max(list_kemiripan))] == 'nan' else ganti_[list_kemiripan.index(max(list_kemiripan))] #'Yes' if fruit == 'Apple' else 'No'
+                last_use_k.append(kt)
+                last_use_r.append(komentar_split[indx])
             else:
                 if cek == True:
-                    print("similarity =>",kt,"|",kata_[list_kemiripan.index(max(list_kemiripan))],"=>", str(max(list_kemiripan)))
-    return " ".join(komentar_split)
+                    pass
+                    #print("similarity =>",kt,"|",kata_[list_kemiripan.index(max(list_kemiripan))],"=>", str(max(list_kemiripan)))
+    ret = re.sub(' +', ' '," ".join(komentar_split))
+    return ret.strip()
